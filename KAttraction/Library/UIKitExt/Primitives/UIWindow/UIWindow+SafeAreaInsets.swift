773ef4ddfd12d924fc8f68e6e2b13e6d0e949f6d
Alexandra Kondrat'eva <Kond1ateff@yandex.ru>
2022-04-08 11:30:34 +0200

    Code refactoring

--- KAttraction/Library/UIKitExt/Primitives/UIWindow/UIWindow+SafeAreaInsets.swift
+++ KAttraction/Library/UIKitExt/Primitives/UIWindow/UIWindow+SafeAreaInsets.swift
@@ -2,12 +2,7 @@ import UIKit
 
 public extension UIWindow {
     static let safeAreaInsets: UIEdgeInsets = {
-        let window = UIApplication.shared.keyWindow
-        if #available(iOS 11.0, *) {
-            return window?.safeAreaInsets ?? .zero
-        } else {
-            return .zero
-        }
+        UIWindow.safeAreaInsets
     }()
 
     static let safeAreaBottomInset: CGFloat = {
